57b0ade5f6a9394481f96ce452dbed342db029ba
elverdavid <elverdavid0839@gmail.com>
2022-12-20 13:45:00 -0500

    3 commit

--- src/dataBase.js
+++ src/dataBase.js
@@ -4,6 +4,7 @@ import {cyan} from 'console-log-colors'
 config();
 
 //conexion a mongoDB atlas
+mongoose.set('strictQuery', true)
 mongoose.connect(process.env.mongodb_url)
 .then(()=>console.log(cyan.bold("successful connection to the database mongodb Atlas")))
 .catch((error)=>console.log(error))
